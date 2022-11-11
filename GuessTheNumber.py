a3ab61f1be685427d8a29470ca80f7b33c64f8d3
KunalManna <117971514+KunalManna@users.noreply.github.com>
2022-11-11 13:35:02 +0530

    Update GuessTheNumber.py

--- GuessTheNumber.py
+++ GuessTheNumber.py
@@ -1,9 +1,9 @@
 import random
-num=random.randint(1,100)
+num=random.randint(1,100) #Wil generate random number between 1 to 100
 guess=None
-nguess=1
+nguess=1                  #Counter Variable for count the attempt by the user
 while guess!=num:
-    guess=int(input("Enter the number between 1 to 100:\n"))
+    guess=int(input("Enter the number between 1 to 100:\n"))   #User input
     if guess>100:
         print("Please give the proper input.")
     elif guess>num:
